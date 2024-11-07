72236fb1d01d42e74daa720d0f9ccfd3b3375599
yuu111 <yuu210yuu@gmail.com>
2024-11-07 22:18:52 +0900

    init

---
+++ Plugin.cs
@@ -0,0 +1,24 @@
+﻿using BepInEx;
+using HarmonyLib;
+using BepInEx.Logging;
+
+
+namespace WidgetTimeViwer
+{
+    [BepInPlugin(pluginGuid, pluginName, pluginVersion)]
+    public class Plugin : BaseUnityPlugin
+    {
+        // const
+        public const string pluginGuid = "com.github.yuu1111.widgettimeviwer";
+        public const string pluginName = "WidgetTimeViwer Mod";
+        public const string pluginVersion = "1.0.0";
+
+        internal static new ManualLogSource Logger;
+
+        public void Awake()
+        {
+            Logger = base.Logger;
+            new Harmony(pluginGuid).PatchAll();
+        }
+    }
+}
---
+++ TextChange.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+using System;
+using static Date;
+
+namespace TimeDetailMod
+{
+
+    [HarmonyPatch(typeof(Date))]
+    [HarmonyPatch(typeof(Date), "GetText", new Type[] { typeof(TextFormat) })]
+    static class TextChange
+    {
+        static bool Prefix(Date __instance, TextFormat format, ref string __result)
+        {
+            if (format == TextFormat.Widget)
+            {
+                __result = "dateYearMonthDay".lang(__instance.year.ToString() ?? "", __instance.month.ToString() ?? "", __instance.day.ToString() ?? "", __instance.hour.ToString() ) + " " + __instance.hour.ToString() + "時" + __instance.min.ToString() + "分";
+                return false;
+            }
+            return true;
+        }
+    }
+}
